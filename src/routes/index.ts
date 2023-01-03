e4124a1fb926d0b3a74e614c27aa270941156577
naughtan <akpanuwah.nsikak@yahoo.com>
2023-01-03 14:04:20 +0100

    chore: my index files edited

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,3 @@
-// import express from 'express';
-// import images from './api/images';
-// const routes = express.Router();
-
-// routes.get('/', (req, res) => {
-//   res.send('main api route');
-// });
-
-// routes.use('/images', images);
-// export default routes;
-
 import express from 'express';
 import images from './api/images';
 import listImagesRouter from './api/imagelist';
